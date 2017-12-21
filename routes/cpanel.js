cf40f808c01c99a7c9046ca40ff51a18b2d12e7b
ankit16-19 <ankitsocial16@gmail.com>
2017-12-21 19:53:46 +0530

    route /files /downloadFile complete

--- routes/cpanel.js
+++ routes/cpanel.js
@@ -21,8 +21,6 @@ router.get('/static', function(req, res, next) {
             res.json(data)
         })
     }
-
-
 });
 router.get('/dynamic', function(req, res, next) {
     var obj = {
@@ -98,10 +96,37 @@ router.get('/dynamic', function(req, res, next) {
         });
 
     }
-
-
-
-
 });
 
+router.get('/files', function(req, res, next) {
+    var obj = [];
+    getdirData(req.query.dir);
+    function getdirData(dir) {
+        shell.cd();
+        shell.cd(dir)
+        var filesDetails = shell.exec('ls -l').split('\n').filter(function (value) {
+            return value != '';
+        });
+        var fileName = shell.exec('ls').split('\n').filter(function (value) {
+            return value != '';
+        });
+        obj.push({
+            fileName,filesDetails
+        });
+        res.json(obj)
+    }
+});
+
+router.get('/downloadFile', function(req, res, next) {
+    downloadFile(req.query.dir, req.query.file);
+    function downloadFile (dir, file) {
+        shell.cd();
+        shell.cd(dir);
+        var dir = shell.exec('pwd');
+        res.sendFile(dir.trim() +"/"+ file.replace(" /g","\\"));
+    }
+});
+
+
 module.exports = router;
+
